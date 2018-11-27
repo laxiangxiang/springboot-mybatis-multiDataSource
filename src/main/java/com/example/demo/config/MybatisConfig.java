49bc9cdd2c6c76b6de559e75eb1c6c53a076cef7
傅俊阳 <fujunyang@shmingjiang.sh.cn>
2018-11-27 12:59:42 +0800

    更多配置

--- src/main/java/com/example/demo/config/MybatisConfig.java
+++ src/main/java/com/example/demo/config/MybatisConfig.java
@@ -24,6 +24,17 @@ public class MybatisConfig {
 
     private static Properties properties = null;
 
+//////////////////把mybatis.properties文件中的属性值传入SQLSessionFactory中的两种方法///////////////////////////////////////////////////////////////////////
+//     * 方法一：使用java配置。在config.java中获取mybatis.properties文件的Properties对象，在创建SQLSessionFactory时把他作为参数传入。                         //
+//     * 方法二：在mybatis-config.xml配置文件中使用<properties><properties/>标签中的resource或者url传入。并且可以覆盖mybatis.properties中的属性。            //
+//     * 方法三：在mybatis-config.xml配置文件中使用<properties><properties/>标签中的子标签<properties name= value= />设置属性值                          //
+//     * 优先级：                                                                                                                                //
+//          在 properties 元素体内指定的属性首先被读取。(最低)                                                                                       //
+//          然后根据 properties 元素中的 resource 属性读取类路径下属性文件或根据 url 属性指定的路径读取属性文件，并覆盖已读取的同名属性。 （中等）              //
+//          最后读取作为方法参数传递的属性，并覆盖已读取的同名属性。（最高）                                                                            //
+//          因此，通过方法参数传递的属性具有最高优先级，resource/url 属性中指定的配置文件次之，最低优先级的是 properties 属性中指定的属性                    //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     static {
         try {
             properties = Resources.getResourceAsProperties(mybatisProperties);
@@ -34,7 +45,10 @@ public class MybatisConfig {
     @Bean(name = "mysqlSqlSessionFactory")
     public SqlSessionFactory getMysqlSqlSessionFactory() throws IOException{
         InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,"mysql",properties);
+        //方法一
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,"mysql",properties);
+        //方法二
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,"mysql");
         return sqlSessionFactory;
     }
 
@@ -42,7 +56,10 @@ public class MybatisConfig {
     @Primary//如果没有使用@Qualify注解指定使用哪一个，优先使用被@Primary注解的bean
     public SqlSessionFactory getOracleSqlSessionFactory() throws IOException{
         InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,"oracle",properties);
+        //方法一
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,"oracle",properties);
+        //方法二
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,"oracle");
         return sqlSessionFactory;
     }
 }
